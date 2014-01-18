8fdc5ad97b9c5428588fdbd7505d759e9bb8abe8
Diego Waxemberg <diego.waxemberg@gmail.com>
2014-01-18 00:56:46 -0500

    added basic UI components and outlets/actions

--- Limelight-iOS/AppDelegate.m
+++ Limelight-iOS/AppDelegate.m
@@ -16,10 +16,6 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
-    // Override point for customization after application launch.
-    self.window.backgroundColor = [UIColor whiteColor];
-    [self.window makeKeyAndVisible];
     return YES;
 }
 
