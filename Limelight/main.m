457b6b13cc266129b724b44aaa55479cb92bba7a
Diego Waxemberg <diego.waxemberg@gmail.com>
2014-08-30 22:46:31 -0400

    Initial Commit

---
+++ Limelight/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Limelight
+//
+//  Created by Diego Waxemberg on 8/30/14.
+//  Copyright (c) 2014 Limelight Stream. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ LimelightTests/LimelightTests.m
@@ -0,0 +1,34 @@
+//
+//  LimelightTests.m
+//  LimelightTests
+//
+//  Created by Diego Waxemberg on 8/30/14.
+//  Copyright (c) 2014 Limelight Stream. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface LimelightTests : XCTestCase
+
+@end
+
+@implementation LimelightTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
