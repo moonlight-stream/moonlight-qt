5f800b79019b4f0942fe111a23d176f8740ae725
Diego Waxemberg <diego.waxemberg@gmail.com>
2014-11-26 23:51:43 -0500

    Added scrolling and right click

--- Limelight/Input/StreamView.m
+++ Limelight/Input/StreamView.m
@@ -19,36 +19,64 @@
     touchLocation = [touch locationInView:self];
     touchMoved = false;
     
-    printf("Touch down\n");
+    NSLog(@"Touch down");
 }
 
 - (void)touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event {
-    UITouch *touch = [[event allTouches] anyObject];
-    CGPoint currentLocation = [touch locationInView:self];
     
-    if (touchLocation.x != currentLocation.x ||
-        touchLocation.y != currentLocation.y)
-    {
-        LiSendMouseMoveEvent(currentLocation.x - touchLocation.x,
-                             currentLocation.y - touchLocation.y );
+    if ([[event allTouches] count] == 1) {
+        UITouch *touch = [[event allTouches] anyObject];
+        CGPoint currentLocation = [touch locationInView:self];
+
+        if (touchLocation.x != currentLocation.x ||
+            touchLocation.y != currentLocation.y)
+        {
+            LiSendMouseMoveEvent(currentLocation.x - touchLocation.x,
+                                 currentLocation.y - touchLocation.y );
+            
+            touchMoved = true;
+            touchLocation = currentLocation;
+        }
+    } else if ([[event allTouches] count] == 2) {
+        CGPoint firstLocation = [[[[event allTouches] allObjects] objectAtIndex:0] locationInView:self];
+        CGPoint secondLocation = [[[[event allTouches] allObjects] objectAtIndex:1] locationInView:self];
         
+        CGPoint avgLocation = CGPointMake((firstLocation.x + secondLocation.x) / 2, (firstLocation.y + secondLocation.y) / 2);
+        if (touchLocation.y != avgLocation.y) {
+            LiSendScrollEvent(avgLocation.y - touchLocation.y);
+        }
         touchMoved = true;
-        touchLocation = currentLocation;
+        touchLocation = avgLocation;
     }
+    
 }
 
 - (void)touchesEnded:(NSSet *)touches withEvent:(UIEvent *)event {
-    printf("Touch up\n");
+    NSLog(@"Touch up");
+    
     
     if (!touchMoved) {
-        printf("Sending left mouse button press\n");
-        
-        LiSendMouseButtonEvent(BUTTON_ACTION_PRESS, BUTTON_LEFT);
-        
-        // Wait 100 ms to simulate a real button press
-        usleep(100 * 1000);
-        
-        LiSendMouseButtonEvent(BUTTON_ACTION_RELEASE, BUTTON_LEFT);
+        if ([[event allTouches] count]  == 2) {
+            NSLog(@"Sending right mouse button press");
+            
+            LiSendMouseButtonEvent(BUTTON_ACTION_PRESS, BUTTON_RIGHT);
+            
+            // Wait 100 ms to simulate a real button press
+            usleep(100 * 1000);
+            
+            LiSendMouseButtonEvent(BUTTON_ACTION_RELEASE, BUTTON_RIGHT);
+            
+            
+        } else {
+            NSLog(@"Sending left mouse button press");
+            
+            LiSendMouseButtonEvent(BUTTON_ACTION_PRESS, BUTTON_LEFT);
+            
+            // Wait 100 ms to simulate a real button press
+            usleep(100 * 1000);
+            
+            LiSendMouseButtonEvent(BUTTON_ACTION_RELEASE, BUTTON_LEFT);
+        }
     }
 }
 
