d134ff83d6a97a0e95d5223eb07b0241cf69444a
Diego Waxemberg <diego.waxemberg@gmail.com>
2015-01-02 23:57:49 -0500

    mDNS discovered hosts have the name set to the address until they are polled

--- Limelight/Network/MDNSManager.m
+++ Limelight/Network/MDNSManager.m
@@ -52,6 +52,7 @@ static NSString* NV_SERVICE_TYPE = @"_nvstream._tcp";
         if (service.hostName != nil) {
             Host* host = [dataMan createHost];
             host.address = service.hostName;
+            host.name = host.address;
             [hosts addObject:host];
         }
     }
