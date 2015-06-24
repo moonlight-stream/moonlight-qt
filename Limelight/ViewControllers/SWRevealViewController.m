175956592053ddb41f8e518d78e1f81c19e54c57
Cameron Gutman <aicommander@gmail.com>
2015-06-23 22:01:59 -0700

    Removed supportedInterfaceOrientations since it didn't do anything and caused a warning on iOS 9 because the prototype seems to have changed

--- Limelight/ViewControllers/SWRevealViewController.m
+++ Limelight/ViewControllers/SWRevealViewController.m
@@ -742,15 +742,6 @@ const int FrontViewPositionNone = 0xff;
     _userInteractionStore = _contentView.userInteractionEnabled;
 }
 
-
-- (NSUInteger)supportedInterfaceOrientations
-{
-    // we could have simply not implemented this, but we choose to call super to make explicit that we
-    // want the default behavior.
-    return [super supportedInterfaceOrientations];
-}
-
-
 #pragma mark - Public methods and property accessors
 
 - (void)setFrontViewController:(UIViewController *)frontViewController
