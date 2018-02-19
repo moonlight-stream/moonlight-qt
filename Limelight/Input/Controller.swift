127e5ed54ad2cd2e2b5b6b55e96a2e18d8e5a72b
Cameron Gutman <aicommander@gmail.com>
2018-02-19 14:43:50 -0800

    Migrate to Swift 4.0 with @objc inferencing fixes for Moonlight build target

--- Limelight/Input/Controller.swift
+++ Limelight/Input/Controller.swift
@@ -14,13 +14,13 @@ import Foundation
  */
 class Controller: NSObject {
   // Swift requires initial properties
-  var playerIndex: CInt = 0           // Controller number (e.g. 1, 2 ,3 etc)
-  var lastButtonFlags: CInt = 0
-  var emulatingButtonFlags: CInt = 0
-  var lastLeftTrigger: CChar = 0      // Last left trigger pressed
-  var lastRightTrigger: CChar = 0     // Last right trigger pressed
-  var lastLeftStickX: CShort = 0      // Last X direction the left joystick went
-  var lastLeftStickY: CShort = 0      // Last Y direction the left joystick went
-  var lastRightStickX: CShort = 0     // Last X direction the right joystick went
-  var lastRightStickY: CShort = 0     // Last Y direction the right joystick went
+  @objc var playerIndex: CInt = 0           // Controller number (e.g. 1, 2 ,3 etc)
+  @objc var lastButtonFlags: CInt = 0
+  @objc var emulatingButtonFlags: CInt = 0
+  @objc var lastLeftTrigger: CChar = 0      // Last left trigger pressed
+  @objc var lastRightTrigger: CChar = 0     // Last right trigger pressed
+  @objc var lastLeftStickX: CShort = 0      // Last X direction the left joystick went
+  @objc var lastLeftStickY: CShort = 0      // Last Y direction the left joystick went
+  @objc var lastRightStickX: CShort = 0     // Last X direction the right joystick went
+  @objc var lastRightStickY: CShort = 0     // Last Y direction the right joystick went
 }
