004e6913fa8a66d21b34ae79ac82b9fece8e2cdb
R. Aidan Campbell <aidan.campbell@gmail.com>
2018-04-30 19:52:59 -0700

    Fixed menu bar being merged with native in osx

--- main.cpp
+++ main.cpp
@@ -3,9 +3,14 @@
 
 int main(int argc, char *argv[])
 {
+    // MacOS directive to prevent the menu bar from being merged into the native bar
+    // i.e. it's in the window, and not the top left of the screen
+    QCoreApplication::setAttribute(Qt::AA_DontUseNativeMenuBar);
+
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
 
     return a.exec();
+
 }
