03dc563b329970a378f7451f5b461181d4e1a403
Cameron Gutman <aicommander@gmail.com>
2018-07-24 21:07:57 -0700

    Create Moonlight png for WiX bootstrapper

--- scripts/generate-ico.sh
+++ scripts/generate-ico.sh
@@ -5,5 +5,6 @@
 # that the output has a transparent background.
 
 convert -density 256 -background none -define icon:auto-resize ../app/res/moonlight.svg ../app/moonlight.ico
+convert -density 256 -background none -size 64x64 ../app/res/moonlight.svg ../app/moonlight_wix.png
 
 echo IMPORTANT: Validate the icon has a transparent background before committing!
