2bcc15e1d290722cfc1d9d896a491fec577a6607
Cameron Gutman <aicommander@gmail.com>
2018-08-04 02:12:30 -0700

    Rebuild FFmpeg for Windows to reduce install size

---
+++ scripts/build-ffmpeg-win64.sh
@@ -0,0 +1,3 @@
+./configure --toolchain=msvc --enable-pic --enable-shared --disable-static --disable-all --enable-avcodec --enable-decoder=h264 --enable-decoder=hevc --enable-hwaccel=h264_dxva2 --enable-hwaccel=hevc_dxva2
+make clean
+make -j4
