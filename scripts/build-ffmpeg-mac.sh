8f8532921cea9a948a8067d3d102631a4151142f
Cameron Gutman <aicommander@gmail.com>
2019-01-07 21:39:59 -0800

    Update FFmpeg build script for Mac to avoid manual steps

--- scripts/build-ffmpeg-mac.sh
+++ scripts/build-ffmpeg-mac.sh
@@ -1,3 +1,7 @@
-./configure --extra-cflags="-mmacosx-version-min=10.10" --enable-pic --enable-shared --disable-static --disable-all --enable-avcodec --enable-decoder=h264 --enable-decoder=hevc --enable-hwaccel=h264_videotoolbox --enable-hwaccel=hevc_videotoolbox
+./configure --extra-cflags="-mmacosx-version-min=10.11" --install-name-dir=@loader_path/../lib --enable-pic --enable-shared --disable-static --disable-all --enable-avcodec --enable-decoder=h264 --enable-decoder=hevc --enable-hwaccel=h264_videotoolbox --enable-hwaccel=hevc_videotoolbox
 make clean
 make -j4
+codesign -s 45U78722YL libavcodec/libavcodec.dylib
+codesign -s 45U78722YL libavutil/libavutil.dylib
+codesign -vv libavcodec/libavcodec.dylib
+codesign -vv libavutil/libavutil.dylib
