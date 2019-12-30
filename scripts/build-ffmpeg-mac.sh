8cefdeae4982631a23d762e807c054d6b2232d94
Cameron Gutman <aicommander@gmail.com>
2019-12-30 02:10:26 -0600

    Update macOS builds to FFmpeg 4.2.1

--- scripts/build-ffmpeg-mac.sh
+++ scripts/build-ffmpeg-mac.sh
@@ -1,7 +1,10 @@
-./configure --extra-cflags="-mmacosx-version-min=10.11" --install-name-dir=@loader_path/../lib --enable-pic --enable-shared --disable-static --disable-all --enable-avcodec --enable-decoder=h264 --enable-decoder=hevc --enable-hwaccel=h264_videotoolbox --enable-hwaccel=hevc_videotoolbox
+rm -rf /tmp/ffmpeg
+mkdir /tmp/ffmpeg
+./configure --prefix=/tmp/ffmpeg --extra-cflags="-mmacosx-version-min=10.13" --install-name-dir=@loader_path/../Frameworks --enable-pic --enable-shared --disable-static --disable-all --enable-avcodec --enable-decoder=h264 --enable-decoder=hevc --enable-hwaccel=h264_videotoolbox --enable-hwaccel=hevc_videotoolbox
 make clean
 make -j4
-codesign -s 45U78722YL libavcodec/libavcodec.dylib
-codesign -s 45U78722YL libavutil/libavutil.dylib
+codesign -s "$SIGNING_IDENTITY" libavcodec/libavcodec.dylib
+codesign -s "$SIGNING_IDENTITY" libavutil/libavutil.dylib
 codesign -vv libavcodec/libavcodec.dylib
 codesign -vv libavutil/libavutil.dylib
+make install
