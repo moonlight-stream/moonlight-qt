174983ce1e2087b30625a91ff6b8b7374c31c3b9
Cameron Gutman <aicommander@gmail.com>
2019-12-31 22:35:28 -0600

    Update Windows builds to FFmpeg 4.2.1 with D3D11VA+DXVA2 enabled

--- scripts/build-ffmpeg-win64.sh
+++ scripts/build-ffmpeg-win64.sh
@@ -1,3 +1,3 @@
-./configure --toolchain=msvc --enable-pic --enable-shared --disable-static --disable-all --enable-avcodec --enable-decoder=h264 --enable-decoder=hevc --enable-hwaccel=h264_dxva2 --enable-hwaccel=hevc_dxva2
+./configure --toolchain=msvc --enable-pic --enable-shared --disable-static --disable-all --enable-avcodec --enable-decoder=h264 --enable-decoder=hevc --enable-hwaccel=h264_dxva2 --enable-hwaccel=hevc_dxva2 --enable-hwaccel=h264_d3d11va --enable-hwaccel=hevc_d3d11va --enable-hwaccel=h264_d3d11va2 --enable-hwaccel=hevc_d3d11va2
 make clean
 make -j4
