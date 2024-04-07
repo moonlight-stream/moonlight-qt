c6710f93dcd57b9179f18e01a6cc46e071d99099
Cameron Gutman <aicommander@gmail.com>
2024-04-06 20:03:54 -0500

    Enable Steam Controller detection on Steam Link
    Fixes #697

--- app/deploy/steamlink/moonlight.sh
+++ app/deploy/steamlink/moonlight.sh
@@ -5,6 +5,10 @@
 # pairing data will be lost each reboot.
 HOME=/usr/local/moonlight
 
+# Enable Steam Controller detection in SDL
+# https://github.com/moonlight-stream/moonlight-qt/issues/697
+export SDL_ENABLE_STEAM_CONTROLLERS=1
+
 # Renice PE_Single_CPU which seems to host A/V stuff
 renice -10 -p $(pidof PE_Single_CPU)
 
