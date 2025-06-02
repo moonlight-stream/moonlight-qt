25f6c020b65d1b9c7973b47f02ac9e0a0232eb36
Batıkan Ökten <xdieofgo@gmail.com>
2025-06-02 06:24:25 +0200

    Translated using Weblate (Turkish)
    Currently translated at 100.0% (250 of 250 strings)
    
    Translation: Moonlight Game Streaming/moonlight-qt
    Translate-URL: https://hosted.weblate.org/projects/moonlight/moonlight-qt/tr/

--- app/languages/qml_tr.ts
+++ app/languages/qml_tr.ts
@@ -48,7 +48,7 @@
     <message>
         <location filename="../gui/AppView.qml" line="348"/>
         <source>This computer doesn&apos;t seem to have any applications or some applications are hidden</source>
-        <translation type="unfinished"></translation>
+        <translation>Bu bilgisayarda hiç uygulama yok gibi görünüyor ya da bazı uygulamalar gizlenmiş</translation>
     </message>
     <message>
         <location filename="../gui/AppView.qml" line="361"/>
@@ -779,7 +779,7 @@
     <message>
         <location filename="../gui/SettingsView.qml" line="718"/>
         <source>Use Default (%1 Mbps)</source>
-        <translation type="unfinished"></translation>
+        <translation>Varsayılanı Kullan (%1 Mbps)</translation>
     </message>
     <message>
         <location filename="../gui/SettingsView.qml" line="753"/>
