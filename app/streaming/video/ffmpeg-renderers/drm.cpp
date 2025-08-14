9dc94bbf8d5ee43938ef1fa6809c21c04c489ede
armin-25689 <83564821+armin-25689@users.noreply.github.com>
2025-08-14 22:08:19 +0800

    build: fix no <linux/dma-buf.h> for BSDs

--- app/streaming/video/ffmpeg-renderers/drm.cpp
+++ app/streaming/video/ffmpeg-renderers/drm.cpp
@@ -12,7 +12,23 @@ extern "C" {
 }
 
 #include <libdrm/drm_fourcc.h>
+#ifdef __linux__
 #include <linux/dma-buf.h>
+#else //bundle on BSDs
+typedef uint64_t __u64;
+struct dma_buf_sync {
+    __u64 flags;
+};
+#define DMA_BUF_SYNC_READ      (1 << 0)
+#define DMA_BUF_SYNC_WRITE     (2 << 0)
+#define DMA_BUF_SYNC_RW        (DMA_BUF_SYNC_READ | DMA_BUF_SYNC_WRITE)
+#define DMA_BUF_SYNC_START     (0 << 2)
+#define DMA_BUF_SYNC_END       (1 << 2)
+#define DMA_BUF_SYNC_VALID_FLAGS_MASK \
+    (DMA_BUF_SYNC_RW | DMA_BUF_SYNC_END)
+#define DMA_BUF_BASE		'b'
+#define DMA_BUF_IOCTL_SYNC	_IOW(DMA_BUF_BASE, 0, struct dma_buf_sync)
+#endif
 
 // Special Rockchip type
 #ifndef DRM_FORMAT_NA12
