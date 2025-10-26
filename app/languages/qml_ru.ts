2f6e7608f85c1c0967da28db7db37d717210d271
arrhteeime <artemlander0003@gmail.com>
2025-10-26 17:38:20 +0100

    Translated using Weblate (Russian)
    Currently translated at 100.0% (251 of 251 strings)
    
    Translation: Moonlight Game Streaming/moonlight-qt
    Translate-URL: https://hosted.weblate.org/projects/moonlight/moonlight-qt/ru/

--- app/languages/qml_ru.ts
+++ app/languages/qml_ru.ts
@@ -885,7 +885,7 @@
     <message>
         <location filename="../gui/SettingsView.qml" line="1242"/>
         <source>Show configuration warnings</source>
-        <translation type="unfinished"></translation>
+        <translation>Ошибки конфигурации</translation>
     </message>
     <message>
         <location filename="../gui/SettingsView.qml" line="1298"/>
